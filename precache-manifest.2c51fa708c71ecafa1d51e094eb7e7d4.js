5ba4668c1c7e6c1a2b0dae73178970f791659c6e
nir099 <a.m.s.nirmal.a@gmail.com>
2019-05-03 21:15:04 +0530

    Updates

---
+++ precache-manifest.2c51fa708c71ecafa1d51e094eb7e7d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d7522dad9c90c7ca822f8e44431250",
+    "url": "/zy-cy/index.html"
+  },
+  {
+    "revision": "f959105220d14ddf5818",
+    "url": "/zy-cy/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/zy-cy/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f959105220d14ddf5818",
+    "url": "/zy-cy/static/js/main.4311b0fe.chunk.js"
+  },
+  {
+    "revision": "aaaab36aa93072c797fd",
+    "url": "/zy-cy/static/js/runtime~main.c1bf237b.js"
+  }
+]);
